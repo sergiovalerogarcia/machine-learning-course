aac99abb942f8882f24b4054ae851f4d5ca94683
Sergio Valero García <sergiovalero92@gmail.com>
2021-10-20 18:00:13 +0200

    Update generate-book.sh

--- generate-book.sh
+++ generate-book.sh
@@ -3,6 +3,6 @@
 #echo "html:" >> _config.yml
 #echo "  google_analytics_id: ${GOOGLE_ANALYTICS_KEY}" >> _config.yml
 
-jupyter-book toc notebooks-library/
+jupyter-book toc from-project ./notebooks-library/ > ./notebooks-library/_toc.yml
 
 jupyter-book build notebooks-library/
